57fed4faae2f31dbb4640342a1c2fdd562d73a0d
Martin Boekhoff <zorbing@gmx.net>
2016-09-12 18:13:43 +0200

    add test file

---
+++ src/test/gKey.ts
@@ -0,0 +1,44 @@
+import * as lgsdk from '../index';
+
+function exitHandler(options: { cleanup?: boolean; exit?: boolean }, error: any)
+{
+	if (options.cleanup)
+	{
+		console.log('shutting down...');
+		lgsdk.gKey.shutdown();
+	}
+	if (error)
+	{
+		console.log(error.stack);
+	}
+	if (options.exit)
+	{
+		process.exit();
+	}
+}
+
+// do something when app is closing
+process.on('exit', exitHandler.bind(null,{ cleanup: true }));
+// catch ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+// catch uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
+lgsdk.gKey.init();
+// lgsdk.gKey.init((gkeyCode, gkeyOrButtonString, context) =>
+// {
+// 	console.log('initiated:', gkeyCode, gkeyOrButtonString, context);
+// });
+
+let lastState = false;
+setInterval(() =>
+{
+	let state = lgsdk.gKey.isKeyboardGkeyPressed(1, 1);
+	if (state !== lastState)
+	{
+		lastState = state;
+		console.log('changed state!', state);
+	}
+}, 100);
+
+process.stdin.resume();
