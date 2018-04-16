31739188bff2f2aafd73c7f708512f64945febef
Martin Boekhoff <zorbing@gmx.net>
2018-04-16 19:28:18 +0200

    add api version to dll names

--- src/path.ts
+++ src/path.ts
@@ -1,3 +1,6 @@
+const pckg = require('../package.json');
+
+
 let pathMap = {
 	 'ia32': 'x86'
 	, 'x64': 'x64'
@@ -6,9 +9,14 @@ if (!pathMap.hasOwnProperty(process.arch))
 {
 	throw new Error(`Architecture ${process.arch} not supported.`)
 }
-export function libPath(type: string)
+
+export function libPath(type: string, version?: string)
 {
+	if (version === undefined)
+	{
+		version = pckg.config[type];
+	}
 	type = type[0].toUpperCase() + type.slice(1).toLowerCase();
 	let arch = pathMap[process.arch];
-	return `./lib/${arch}/Logitech${type}EnginesWrapper`;
+	return `./lib/${arch}/Logitech${type}EnginesWrapper-${version}`;
 }
