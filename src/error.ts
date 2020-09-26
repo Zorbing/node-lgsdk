99bbeb2c3cde5ee95c80918baca3e0d1ef765105
Martin Boekhoff <zorbing@gmx.net>
2020-09-26 20:03:16 +0200

    fix: subscribe exit handler only once

--- src/error.ts
+++ src/error.ts
@@ -1,29 +1,35 @@
-export function getDestroyPromise()
+export const getDestroyPromise = (() =>
 {
-	return new Promise<void>((resolve, reject) =>
+	const promiseList: { resolve: (value?: void | PromiseLike<void> | undefined) => void, reject: (reason?: any) => void }[] = [];
+	function exitHandler(options: { cleanup?: boolean; exit?: boolean }, error: any)
 	{
-		function exitHandler(options: { cleanup?: boolean; exit?: boolean }, error: any)
+		if (options.cleanup)
 		{
-			if (options.cleanup)
-			{
-				resolve();
-			}
-			if (error)
-			{
-				console.error('error:', error);
-				reject(error);
-			}
-			if (options.exit)
-			{
-				process.exit();
-			}
+			promiseList.forEach(promise => promise.resolve());
 		}
+		if (error)
+		{
+			console.error('error:', error);
+			promiseList.forEach(promise => promise.reject(error));
+		}
+		if (options.exit)
+		{
+			process.exit();
+		}
+	}
 
-		// do something when app is closing
-		process.on('exit', exitHandler.bind(null, { cleanup: true }));
-		// catch ctrl+c event
-		process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-		// catch uncaught exceptions
-		process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
-	});
-}
+	// do something when app is closing
+	process.on('exit', exitHandler.bind(null, { cleanup: true }));
+	// catch ctrl+c event
+	process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+	// catch uncaught exceptions
+	process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
+	return function ()
+	{
+		return new Promise<void>((resolve, reject) =>
+		{
+			promiseList.push({ resolve, reject });
+		});
+	};
+})();
